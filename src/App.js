5f1f331f432ee262a844815e0ec2d7baa5c1d65b
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-11-13 01:01:43 +0530

    updated ui

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ const alchemyRPC =
   "https://polygon-mainnet.g.alchemy.com/v2/CtHqiL3aNzpaAZKwY2aMl3s0ArBkcHpw";
 function App() {
   const retro = {
-  primary: 'black',
-  secondary: 'black',
+  primary: 'white',
+  secondary: 'white',
   interactive: '#f7f7f7',
-  container: '#F7F6F2',
-  module: '#AEAEAE',
-  accent: '#0A0708',
-  outline: 'black',
+  container: '#0A0708',
+  module: '#3F3F3F',
+  accent: '#006DFF',
+  outline: 'white',
   dialog: '#000',
   fontFamily: 'Josefin Sans',
-  borderRadius: 0.8,
+  borderRadius: 0.6,
   }
   return (
     <div className="flex flex-row justify-center items-center mt-[40px]">
-      <div className="Uniswap border-2 border-black box_back ">
+      <div className="Uniswap border-2 border-black rounded-xl">
         <SwapWidget jsonRpcEndpoint={alchemyRPC} theme={retro} />
       </div>
     </div>
